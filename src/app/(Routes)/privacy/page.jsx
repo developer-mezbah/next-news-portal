ee27c472c992b6dd935dd339a6c87e55d5203304
developer.mezbah <developer.mezbah@gmail.com>
2024-01-29 16:43:19 +0600

    privacy and condition

--- src/app/(Routes)/privacy/page.jsx
+++ src/app/(Routes)/privacy/page.jsx
@@ -2,9 +2,10 @@ import PlainLayout from "@/Components/master/PlainLayout";
 import parse from "html-react-parser";
 
 async function getData() {
-  return (
-    await (await fetch(`${process.env.BASE_URL}/api/policy?type=privacy`)).json()
-  )["data"];
+  const data = await (
+    await fetch(`${process.env.BASE_URL}/api/policy?type=privacy`)
+  ).json()
+  return data
 }
 
 const page = async () => {
@@ -15,9 +16,7 @@ const page = async () => {
         <h2 className="text-4xl mt-16 mb-2">
           <span className="text-themeColor font-bold">Privacy</span>
         </h2>
-        <div className="my-5">
-          {parse(data[0]["long_des"])}
-        </div>
+        <div className="my-5">{parse(data?.data[0]["long_des"])}</div>
       </div>
     </PlainLayout>
   );
--- src/app/(Routes)/terms/page.jsx
+++ src/app/(Routes)/terms/page.jsx
@@ -2,9 +2,10 @@ import PlainLayout from "@/Components/master/PlainLayout";
 import parse from "html-react-parser";
 
 async function getData() {
-  return (
-    await (await fetch(`${process.env.BASE_URL}/api/policy?type=terms`)).json()
-  )["data"];
+  const data = await (
+    await fetch(`${process.env.BASE_URL}/api/policy?type=terms`)
+  ).json()
+  return data;
 }
 
 const page = async () => {
@@ -13,11 +14,11 @@ const page = async () => {
     <PlainLayout>
       <div className="container mx-auto">
         <h2 className="text-4xl mt-16 mb-2">
-          <span className="text-themeColor font-bold">Terms and Conditions</span>
+          <span className="text-themeColor font-bold">
+            Terms and Conditions
+          </span>
         </h2>
-        <div className="my-5">
-          {parse(data[0]["long_des"])}
-        </div>
+        <div className="my-5">{parse(data?.data[0]["long_des"])}</div>
       </div>
     </PlainLayout>
   );
